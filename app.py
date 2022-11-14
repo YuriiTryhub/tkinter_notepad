fdb92fa503b44bf9aae205a123c9e984b9bdf656
Yurii Trihub <yuriitrihub@gmail.com>
2022-11-14 15:30:38 +0200

    [upd] add bg app color black

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from tkinter import *
-root = Tk()
+root = Tk() # окно приложения
 # Code to add widgets will go here...
 
+# оновные параметры окна
 root.title('PyMegaPad')     # изменение заголовка прилоржения
 root.geometry('600x400+700+300') # положение приложения на экране и его размеры
 root.iconbitmap('icon.ico') # измнение лого приложения
+root.resizable(True, True) # изменение размеров по высоте и ширине
+# root.config(bg='#000') # цвет фона 
+# root['bg'] = 'black'
+
+# 
 
 root.mainloop()
