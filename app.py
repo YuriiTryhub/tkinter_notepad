d639b2a2eee8c62eb407b9b77aaac4216e7095a5
Yurii Trihub <yuriitrihub@gmail.com>
2022-11-14 14:18:05 +0200

    [upd] add new app logo

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ root = Tk()
 # Code to add widgets will go here...
 
 root.title('PyMegaPad')     # изменение заголовка прилоржения
-
 root.geometry('600x400+700+300') # положение приложения на экране и его размеры
+root.iconbitmap('icon.ico') # измнение лого приложения
 
 root.mainloop()
