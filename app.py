60bd05f0ae52c9797a7f0ca606733633d755d2bf
Yurii Trihub <yuriitrihub@gmail.com>
2022-11-14 14:08:23 +0200

    [upd] app name change, the position of the application on the screen

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from tkinter import *
-top = Tk()
+root = Tk()
 # Code to add widgets will go here...
-top.mainloop()
+
+root.title('PyMegaPad')     # изменение заголовка прилоржения
+
+root.geometry('600x400+700+300') # положение приложения на экране и его размеры
+
+root.mainloop()
